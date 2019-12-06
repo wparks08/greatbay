2be10018c909862d26af4b5758a598322f127174
wparks08 <wparks08@gmail.com>
2019-12-06 00:37:11 -0800

    add ability to post item. Start implementation of bidding on item.

---
+++ Item.js
@@ -0,0 +1,74 @@
+var connection = require("./db");
+
+/**
+ * Constructor for a new Item
+ * @param {object} values 
+ */
+function Item(values) {
+    this.id = values.id || null;
+    this.name = values.name || "";
+    this.highBid = values.highBid || 0;
+    this.highestBidderId = values.highestBidderId || null;
+}
+
+/**
+ * Insert new item into DB
+ * @param {object} values 
+ */
+function create(values) {
+    connection.query(
+        "INSERT INTO item SET ?",
+        values,
+        (error, result) => {
+            console.log(result);
+        }
+    )
+}
+
+//Doesn't work right -- async issues...
+// function read(id) {
+//     connection.query(
+//         "SELECT * FROM item WHERE ?",
+//         { id: id },
+//         (error, result) => {
+//             return new Item(result[0]);
+//         }
+//     )
+// }
+
+/**
+ * Update item with specified id
+ * @param {number} id 
+ * @param {object} values 
+ */
+function update(id, values) {
+    connection.query(
+        "UPDATE item SET ? WHERE ?",
+        [values, { id: id }],
+        (error, result) => {
+            //do nothing?
+        }
+    )
+}
+
+/**
+ * Delete specified object
+ * @param {number} id 
+ */
+function deleteItem(id) {
+    connection.query(
+        "DELETE FROM item WHERE ?",
+        { id: id },
+        (error, result) => {
+            //do nothing?
+        }
+    )
+}
+
+module.exports = {
+    Item: Item,
+    create: create,
+    read: read,
+    update: update,
+    delete: deleteItem
+}
--- index.js
+++ index.js
@@ -1,21 +1,95 @@
-var mysql = require("mysql");
 var inquirer = require("inquirer");
+var Item = require("./Item");
+var connection = require("./db");
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "password",
-    database: "greatbay"
-});
+const POST = "Post an item";
+const BID = "Bid on an item";
 
-connection.connect(err => {
-    if (err) throw err;
-
-    //call loginPrompt() from here
-
-})
-
-function loginPrompt() {
-    //inquirer prompt asking to login or sign up
+//Prompt user if they would like to post an item, or bid on an item
+//--Starting Point--
+function promptAction() {
+    inquirer.prompt([
+        {
+            type: "list",
+            choices: [POST, BID],
+            message: "What would you like to do?",
+            name: "choice"
+        }
+    ]).then(answer => {
+        switch (answer.choice) {
+            case POST:
+                promptCreateItem();
+                break;
+            case BID:
+                promptBidOnItem();
+                break;
+        }
+    });
 }
+
+//Prompts for posting a new item for sale
+function promptCreateItem() {
+    console.log("-------------------");
+    console.log("|  Post New Item  |");
+    console.log("-------------------");
+    inquirer.prompt([
+        {
+            message: "Name: ",
+            name: "name"
+        }
+    ]).then(values => {
+        Item.create(values);
+        promptAction();
+    })
+}
+
+//Prompts for bidding on an item. Shows all items, asks user which one to bid on, and for a bid.
+//If user entered bid is highest, display message and update item in DB
+function promptBidOnItem() {
+    console.log("--------------------");
+    console.log("|  Bid On An Item  |");
+    console.log("--------------------");
+
+    let items = [];
+    let itemChoices = [];
+
+    connection.query(
+        "SELECT * FROM item",
+        (error, result) => {
+            console.log(result);
+            result.forEach(item => {
+                items.push(new Item.Item(item)); //There must be a better way, to avoid "Item.Item()"
+                itemChoices.push(item.id);
+            });
+
+            showAvailableItems(items);
+
+            inquirer.prompt([
+                {
+                    type: "list",
+                    choices: itemChoices,
+                    message: "Choose an item: ",
+                    name: "id"
+                },
+                {
+                    message: "Enter a new bid: ",
+                    name: "bid"
+                }
+            ]).then(choice => {
+                // console.log(choice.id);
+                // console.log(parseFloat(choice.bid));
+            })
+        }
+    )
+}
+
+//Helper function to display results when listing all available items
+function showAvailableItems(items) {
+    console.log("-------------- Available Items --------------");
+    items.forEach(item => {
+        console.log(`${item.id}. ${item.name} (${item.highBid})`);
+    });
+}
+
+//Execute starting point
+promptAction();
